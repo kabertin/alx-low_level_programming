9e8d85033a6905b1f18314d53f06623a66a58800
kabertin <karindabertin35@gmail.com>
2023-09-07 15:36:33 -0700

    yes ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: print a sentence using write function
- *	ssize_t write(int fd, const void *buf, size_t count);
- *
- * Return: 1 (error)
-*/
-
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
