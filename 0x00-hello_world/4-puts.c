3234e20515ca80b3f997c6dd71ee6c5e330f4e56
kabertin <karindabertin35@gmail.com>
2023-09-07 07:48:12 -0700

    write a c program thqt prints a string using puts fonction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*This program will print a string*/
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
