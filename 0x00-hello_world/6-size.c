e91369a29ce74bd37bb7129670774c547830b7ea
kabertin <karindabertin35@gmail.com>
2023-09-07 09:33:07 -0700

    trying to fix my program of sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	printf("Size of char: %zu bytes\n", sizeof(char));
 	printf("Size of int: %zu bytes\n", sizeof(int));
 	printf("Size of long integer: %zu bytes\n", sizeof(long int));
+	printf("Size of long long integer: %zu bytes\n", sizeof(long long int));
 	printf("Size of float: %zu bytes\n", sizeof(float));
 	return (0);
 }
