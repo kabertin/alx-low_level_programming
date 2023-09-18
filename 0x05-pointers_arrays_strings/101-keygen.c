bd05ccaa3ac7ee4a99050bbd575b38a37ae81fc6
kabertin <karindabertin35@gmail.com>
2023-09-18 15:54:50 -0700

    end

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - creating random password
+ *
+ * Return: (0) Success
+ *
+ */
+int main(void)
+{
+	int add;
+	char h;
 
+	srand(time(NULL));
+	while (add <= 2645)
+	{
+		h = rand() % 128;
+		add += h;
+		putchar (h);
+	}
+	putchar(2772 - add);
+	return (0);
+}
