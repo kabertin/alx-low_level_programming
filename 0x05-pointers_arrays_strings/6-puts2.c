6c9417968ba7f046aae78dad5c6b3ff6e042b3c8
kabertin <karindabertin35@gmail.com>
2023-09-18 10:05:09 -0700

    still task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,11 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(*str);
-		str = str + 2;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
