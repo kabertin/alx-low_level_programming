5700cbb463c8784bba526dc9e34faca58cdec091
kabertin <karindabertin35@gmail.com>
2023-10-04 13:28:03 -0700

    task 5

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,5 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 void *malloc_check(unsigned int b);
 char *_strdup(char *str);
-void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 #endif
